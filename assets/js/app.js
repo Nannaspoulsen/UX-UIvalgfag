d263eeebd34ee39177693e1fcc563de3649a797a
Isaskovsende <skovsende45@gmail.com>
2025-09-18 12:13:07 +0200

    Recipe
    Recipe oprettet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
+/*Recipe*/
+
+// Find alle cirkler/tjekbokse
+const circles = document.querySelectorAll('.circle');
+circles.forEach(circle => {
+  circle.addEventListener('click', () => {
+    circle.classList.toggle('checked');
+    circle.nextElementSibling.classList.toggle('checked');
+  });
+});
 
